23e317a02b42090725d1795f15645b6f07eafc73
Isaac Roberts <isc-rbrts@gmail.com>
2024-04-13 23:03:06 -0700

    finished form layout

--- main.py
+++ main.py
@@ -6,15 +6,24 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
+
 @app.route('/', methods =["GET", "POST"])
 def gfg():
     if request.method == "POST":
+       align_type = request.form.get("align_type")
        seq1 = request.form.get("seq1")
-       seq2 = request.form.get("seq2") 
+       seq2 = request.form.get("seq2")
+       match_reward = request.form.get("match_reward")
+       mismatch_penalty = request.form.get("mismatch_penalty")
+       indel_penalty = request.form.get("indel_penalty")
 
-       return "Aligning sequences: " + seq1 + " " + seq2
+       return "Aligning sequences: " + seq1 + " " + seq2 + " with alignment type: " + align_type + " " + str(match_reward) + str(mismatch_penalty) + str(indel_penalty)
     
     return render_template("home.html")
 
-#if __name__ == "__main__":
-#    app.run(host="127.0.0.1", port=8080, debug=True)
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=8080, debug=True)
